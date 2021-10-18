4282697cd01c01b9a379fb60fe2f0fe7cab90f25
connectshark <bobhus394@gmail.com>
2021-10-18 14:48:21 +0800

    充電文字

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,10 @@ export default {
 </script>
 
 <style>
+:root{
+  --progress: 100;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Roboto Mono', 'Noto Sans TC', sans-serif;
 }
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,4 +1,79 @@
 <template>
-  <img alt="Vue logo" src="../assets/logo.png">
-  <h2>Hi! 快速使用vue3做出網頁template</h2>
+<div class="charge">
+  <p class="word">充電人生，充電工作，充電充電充電充電充電充電</p>
+  <div class="btns">
+    <input type="button" value="充電!!" class="btn" @click="start">
+  </div>
+</div>
 </template>
+
+<script>
+import { ref, watch } from 'vue'
+export default {
+  setup () {
+    const progress = ref(100)
+
+    let allowClick = true
+
+    watch(progress, value => {
+      if (value <= 0) {
+        allowClick = true
+        return
+      }
+      allowClick = false
+      setTimeout(() => {
+        progress.value = value - 1
+        setProgress()
+      }, 30)
+    })
+
+    const start = () => {
+      if (allowClick) {
+        if (progress.value === 0) {
+          progress.value = 100
+        } else {
+          --progress.value
+        }
+      }
+    }
+
+    function setProgress () {
+      document.querySelector(':root').style.setProperty(`--progress`, progress.value)
+    }
+
+    return {
+      progress,
+      start
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.charge{
+  background-color: #000;
+  min-height: 100vh;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+  .word{
+    width: 80%;
+    max-width: 600px;
+    margin: 0 auto;
+    color: transparent;
+    font-size: 80px;
+    background-image: linear-gradient(#fff calc(var(--progress) * 1%), #4cd265 calc(var(--progress) * 1%));
+    background-clip: text;
+  }
+  .btns{
+    .btn{
+      display: inline-block;
+      border: none;
+      border-radius: 10px;
+      width: 80px;
+      height: 30px;
+    }
+  }
+}
+</style>
